deb0363d174af044bd543c1bc11a64961b81288a
DongKwon <gun100491@gmail.com>
2021-08-05 23:16:33 +0900

    youtube clone coding

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,3 +1,5 @@
+import axious from "axios";
+
 class Youtube {
   constructor(key) {
     this.key = key;
@@ -12,13 +14,16 @@ class Youtube {
       `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
       this.getRequestOptions
     );
+    const response1 = await axious.get(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}``,)
     const result = await response.json();
     return result.items;
+    
   }
 
   async search(query) {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+      'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
       this.getRequestOptions
     );
     const result = await response.json();
